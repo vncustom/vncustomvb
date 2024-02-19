baeb3ffd1e023b35cacfc41d4f03779165b114dd
vncustom <96513474+vncustom@users.noreply.github.com>
2024-02-19 17:41:18 +0700

    add chinese

---
+++ uukanshu/src/detail.js
@@ -0,0 +1,27 @@
+load("config.js");
+
+function execute(url) {
+    if (url.indexOf("sj.") === -1) {
+        let bookId = url.match(/\/b\/(\d+)\/?$/)[1];
+        url = MOBILE_URL + "/book.aspx?id=" + bookId;
+    }
+    let doc = fetch(url).html();
+    if (doc) {
+        let info = doc.select(".book-info");
+        let coverImg = info.select(".pic img").first().attr("src");
+        if (coverImg.startsWith("//")) {
+            coverImg = "https:" + coverImg;
+        }
+        return Response.success({
+            name: info.select("h1.bookname").text(),
+            cover: coverImg,
+            author: info.select("dd").first().text().replace("作者：", ""),
+            description: info.select(".desc").text(),
+            detail: info.select("dd").html(),
+            host: MOBILE_URL
+        });
+    }
+
+    return null;
+
+}
